eeeb9045b4be14ae41090f0c33edd8003663ca24
Gurp1272 <isaacfinley@gmail.com>
2023-03-09 14:47:46 -0700

    init

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule ExBackblaze.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ex_backblaze,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:finch, "~> 0.14"},
+      {:jason, "~> 1.3"},
+      {:ex_doc, "~> 0.29.2"}
+    ]
+  end
+end
---
+++ test/ex_backblaze_test.exs
@@ -0,0 +1,8 @@
+defmodule ExBackblazeTest do
+  use ExUnit.Case
+  doctest ExBackblaze
+
+  test "greets the world" do
+    assert ExBackblaze.hello() == :world
+  end
+end
